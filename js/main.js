b4fb4eb169bbb5d9da48410d3a22f210c5dfef0a
Harold Velandia <faridvo10@gmail.com>
2024-05-08 22:25:37 -0500

    adding pop up (if something desnt work restore from this commit xd)

--- js/main.js
+++ js/main.js
@@ -79,3 +79,11 @@ finder.addEventListener('input', function(){
         }
     });    
 });
+
+/*=================== POP UP ===================*/
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
