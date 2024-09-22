3c39d550271dd5b45a8da03728e7286921492668
Harold Velandia <158774396+HaroldVelandia@users.noreply.github.com>
2024-09-22 23:27:05 +0000

    Clausura gallery shart

--- js/gallery.js
+++ js/gallery.js
@@ -19,6 +19,11 @@ document.addEventListener('DOMContentLoaded', function() {
             date: '2024-09-07',
             title: '7 de Septiembre 2024',
             photos: ['imagenes/gallery/7septiembre/jul6img1.jpg','imagenes/gallery/6julio/jul6img2.jpg','imagenes/gallery/6julio/jul6img3.jpg','imagenes/gallery/6julio/jul6img4.jpg','imagenes/gallery/6julio/jul6img5.jpg','imagenes/gallery/6julio/jul6img6.jpg','imagenes/gallery/6julio/jul6img7.jpg']
+        },
+        {
+            date: '2024-08-24',
+            title: 'Clausura semana Juvenil',
+            photos: ['imagenes/gallery/7septiembre/jul6img1.jpg','imagenes/gallery/6julio/jul6img2.jpg','imagenes/gallery/6julio/jul6img3.jpg','imagenes/gallery/6julio/jul6img4.jpg','imagenes/gallery/6julio/jul6img5.jpg','imagenes/gallery/6julio/jul6img6.jpg','imagenes/gallery/6julio/jul6img7.jpg']
         }
     ];
 
